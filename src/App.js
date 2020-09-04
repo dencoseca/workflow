3fa7d0941d2da734cbaedd8fc210b9a5e0c44a30
dencoSeca <leon.brown71@gmail.com>
2020-09-04 11:57:13 +0100

    add welcome component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
+
+// COMPONENTS
 import Header from './components/Header'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <>
       <Header />
+      <Welcome />
     </>
   )
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import Page from './Page'
+
+function Welcome() {
+  return (
+    <Page title="Welcome">
+      <div className="welcome has-text-centered mt-6">
+        <h1 className="title is-1">Welcome!</h1>
+        <h5 className="subtitle mt-4">Workflow is here to help you get your projects, and you, organised. Just sign up or log in to get started.</h5>
+      </div>
+    </Page>
+  )
+}
+
+export default Welcome
