060bdefa4c955509327c49f06b989e532a9c5a2b
dencoSeca <leon.brown71@gmail.com>
2020-09-09 13:02:34 +0100

    reinstate project hover highlight on delete icon

--- src/components/ProjectListItem.js
+++ src/components/ProjectListItem.js
@@ -3,15 +3,15 @@ import React from 'react'
 function ProjectListItem(props) {
   return (
     <div className="project-list--item mt-1">
-      <span onClick={e => props.setSelectedProject(props.project._id)} className="project-list--item-name">
-        {props.project.name}
-      </span>
       <span className="icon">
         <i
           onClick={e => props.handleProjectDelete(e)}
-          className="project-list--item-delete fa fa-times has-text-danger"
+          className="project-list--item-delete fa fa-trash has-text-danger"
           data-project={props.project._id}
         ></i>
+      </span>
+      <span onClick={e => props.setSelectedProject(props.project._id)} className="project-list--item-name">
+        {props.project.name}
       </span>
     </div>
   )
