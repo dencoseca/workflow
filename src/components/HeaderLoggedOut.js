6649db761d489b132a16c73a5fe9b676c4c53bb0
dencoSeca <leon.brown71@gmail.com>
2020-09-11 10:42:48 +0100

    move sign up logic from into Welcome component and style

--- src/components/HeaderLoggedOut.js
+++ src/components/HeaderLoggedOut.js
@@ -11,29 +11,6 @@ function HeaderLoggedOut() {
   const appDispatch = useContext(DispatchContext)
 
   useEffect(() => {
-    if (signUpRequestCount > 0) {
-      const ourRequest = axios.CancelToken.source()
-
-      async function signUp() {
-        try {
-          const response = await axios.post('http://localhost:8080/user/signup', { username, password }, { cancelToken: ourRequest.token })
-          if (response.data.errorMessage) {
-            appDispatch({ type: 'flashMessage', value: response.data.errorMessage, color: 'danger' })
-          } else {
-            appDispatch({ type: 'login', value: response.data })
-          }
-        } catch (e) {
-          console.log('There was a problem or the request was cancelled.')
-        }
-      }
-      signUp()
-      return () => {
-        ourRequest.cancel()
-      }
-    }
-  }, [signUpRequestCount])
-
-  useEffect(() => {
     if (loginRequestCount > 0) {
       const ourRequest = axios.CancelToken.source()
 
@@ -66,20 +43,29 @@ function HeaderLoggedOut() {
     <div className="header-logged-out">
       <form onKeyDown={handleKeyDown}>
         <div className="control mr-2">
-          <input onChange={e => setUsername(e.target.value)} value={username} className="input" type="text" name="username" placeholder="username"></input>
+          <input
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+            className="input"
+            type="text"
+            name="username"
+            placeholder="username"
+          ></input>
         </div>
         <div className="control mr-2">
-          <input onChange={e => setPassword(e.target.value)} value={password} className="input" type="password" name="password" placeholder="password"></input>
+          <input
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            className="input"
+            type="password"
+            name="password"
+            placeholder="password"
+          ></input>
         </div>
       </form>
-      <div className="buttons">
-        <button onClick={e => setSignUpRequestCount(prev => prev + 1)} className="button is-primary">
-          <strong>Sign up</strong>
-        </button>
-        <button onClick={e => setLoginRequestCount(prev => prev + 1)} className="button is-primary is-outlined">
-          Log in
-        </button>
-      </div>
+      <button onClick={e => setLoginRequestCount(prev => prev + 1)} className="button is-primary has-text-weight-bold">
+        Log in
+      </button>
     </div>
   )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,98 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import Page from './Page'
+import DispatchContext from '../DispatchContext'
+import axios from 'axios'
 
 function Welcome() {
+  const appDispatch = useContext(DispatchContext)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [signUpRequestCount, setSignUpRequestCount] = useState(0)
+
+  useEffect(() => {
+    if (signUpRequestCount > 0) {
+      const ourRequest = axios.CancelToken.source()
+
+      async function signUp() {
+        try {
+          const response = await axios.post('http://localhost:8080/user/signup', { username, password }, { cancelToken: ourRequest.token })
+          if (response.data.errorMessage) {
+            appDispatch({ type: 'flashMessage', value: response.data.errorMessage, color: 'danger' })
+          } else {
+            appDispatch({ type: 'flashMessage', value: response.data.successMessage, color: 'success' })
+            appDispatch({ type: 'login', value: response.data.user })
+          }
+        } catch (e) {
+          console.log('There was a problem or the request was cancelled.')
+        }
+      }
+      signUp()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [signUpRequestCount])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!username) {
+      appDispatch({ type: 'flashMessage', value: 'Username cannot be blank', color: 'danger' })
+    } else if (!password) {
+      appDispatch({ type: 'flashMessage', value: 'Password cannot be blank', color: 'danger' })
+    } else if (username && password) {
+      setSignUpRequestCount(prev => prev + 1)
+    }
+  }
+
   return (
     <Page title="Welcome">
-      <div className="welcome has-text-centered mt-6">
-        <h1 className="title is-1">Welcome!</h1>
-        <h5 className="subtitle mt-4">Workflow is here to help you get your projects, and you, organised. Just sign up or log in to get started.</h5>
+      <div className="welcome--main-container mt-6 mx-6 columns">
+        <div className="welcome--sub-container column is-one-half">
+          <h1 className="title is-1 mb-6">Welcome!</h1>
+          <p className="subtitle">
+            Create projects and tasks and manage your progress. We think project management should be simple. Just sign up or login to start
+            organising your workflow.
+          </p>
+        </div>
+        <div className="welcome--sub-container column is-one-half">
+          <form onSubmit={e => handleSubmit(e)} className="mx-6">
+            <div className="field">
+              <label htmlFor="username" className="label">
+                Username
+              </label>
+              <div className="control">
+                <input
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  type="text"
+                  id="username"
+                  className="input"
+                  placeholder="Pick a username"
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <label htmlFor="password" className="label">
+                Password
+              </label>
+              <div className="control">
+                <input
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  type="password"
+                  id="password"
+                  className="input"
+                  placeholder="Create a password"
+                ></input>
+              </div>
+            </div>
+            <div className="control">
+              <button className="button is-primary is-large is-fullwidth mt-5">
+                <span className="welcome--bold-button-text has-text-weight-bold">Sign up</span> for Workflow
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </Page>
   )
