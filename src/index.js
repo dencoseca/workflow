8dd03ba6ed73ac0c53f0b9601bd7be72f3fde2ee
dencoSeca <leon.brown71@gmail.com>
2020-09-25 11:02:06 +0100

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import 'bulma/css/bulma.css'
 import './index.scss'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import axios from 'axios'
 axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT
 
@@ -17,7 +16,3 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
